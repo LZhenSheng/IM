b626f5382026fca1c2e075551836b836ec61a716
LZhenSheng <2759100807@qq.com>
2024-09-01 21:50:05 +0800

    更加消息广播和下线通知功能

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ import (
 	"net"
 	"fmt"
 	"sync"
+	"io"
 )
 type Server struct{
 	Ip string
@@ -50,6 +51,25 @@ func (this *Server) Handler(conn net.Conn){
 	this.mapLock.Unlock()
 	//广播当前用户上线消息
 	this.BroadCast(user,"已上线")
+	//接受客户端发送的消息
+	go func(){
+		buf:=make([]byte,4096)
+		for{
+			n,err:=conn.Read(buf)
+			if n==0{
+				this.BroadCast(user,"下线")
+				return
+			}
+			if err!=nil && err!=io.EOF{
+				fmt.Println("Conn Read err:",err)
+				return
+			}
+			//提取用户消息，去除\n
+			msg:=string(buf[:n-1])
+			//将得到的消息进行广播
+			this.BroadCast(user,msg)
+		}
+	}()
 	//当前handler阻塞
 	select{
 
